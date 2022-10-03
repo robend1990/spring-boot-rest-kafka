2da27fae3da43f8d8406b8843fd440629983bbb9
Robert Drewniak <robend1990@vp.pl>
2022-10-03 11:16:44 +0200

    added some more logging

--- src/main/java/com/faceit/assessment/infrastructure/mvc/ExceptionHandlerAdvice.java
+++ src/main/java/com/faceit/assessment/infrastructure/mvc/ExceptionHandlerAdvice.java
@@ -3,6 +3,7 @@ package com.faceit.assessment.infrastructure.mvc;
 import com.faceit.assessment.user.exception.UserAlreadyExistsException;
 import com.faceit.assessment.user.exception.UserNotFoundException;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -14,23 +15,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 @ControllerAdvice
+@Slf4j
 public class ExceptionHandlerAdvice {
 
     @ExceptionHandler(UserNotFoundException.class)
     ResponseEntity<ErrorMessage> handleUserNotFound(UserNotFoundException e) {
+        log.error(e.getMessage());
         ErrorMessage errorMessage = new ErrorMessage(e.getMessage());
         return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(UserAlreadyExistsException.class)
     ResponseEntity<ErrorMessage> handleUserExists(UserAlreadyExistsException e) {
+        log.error(e.getMessage());
         ErrorMessage errorMessage = new ErrorMessage(e.getMessage());
         return new ResponseEntity<>(errorMessage, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     ResponseEntity<List<FieldValidationError>> handleDtoValidationError(MethodArgumentNotValidException e) {
-
+        log.error("Request body is invalid!", e);
         List<FieldValidationError> errors = new ArrayList<>();
         e.getBindingResult().getAllErrors().forEach((error) -> {
 
